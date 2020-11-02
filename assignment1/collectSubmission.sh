fc229aea7394870decd0cec10da128d63d14c32b
Dmitry <d.shereshevskiy@gmail.com>
2020-11-02 19:58:49 +0300

    Initial commit

---
+++ assignment1/collectSubmission.sh
@@ -0,0 +1,2 @@
+rm -f cs231n_assign1_dmitry_sh.zip 
+zip -r cs231n_assign1_dmitry_sh.zip . -x "*.git*" "*cs231n/datasets*" "*.ipynb_checkpoints*" "*README.md" "*collectSubmission.sh" "*requirements.txt" ".env/*"
